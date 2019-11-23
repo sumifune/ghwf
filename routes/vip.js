8ee0d74c6deb8e12edbd6e96b58340b789c6d84f
sumifune <sukia.mifune@gmail.com>
2019-11-23 20:27:36 +0100

    merging2

--- routes/vip.js
+++ routes/vip.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('VIP service');
+  res.send('parallel-api');
 });
 
 module.exports = router;
