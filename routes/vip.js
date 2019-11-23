3f20319031974a02ffeb306022de0c93c6b3ce17
sumifune <sukia.mifune@gmail.com>
2019-11-23 15:43:07 +0100

    add zzz

--- routes/vip.js
+++ routes/vip.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('VIP service bbb');
+  res.send('VIP service bbb zzz');
 });
 
 module.exports = router;
